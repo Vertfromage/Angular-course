4c07a02b344210c46604df455c354aff6ffe1a18
Crystal Parker <crystal@koii.network>
2025-01-11 13:42:32 -0400

    adding a new user

--- 01-starting-project/src/app/tasks/form/form.component.ts
+++ 01-starting-project/src/app/tasks/form/form.component.ts
@@ -1,17 +1,31 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'; // collection of features and directives to help with forms
+import { type NewTaskData } from '../task/task.model';
+// forms model automatically prevents default browser behavior and has a custom event
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
 export class FormComponent {
   @Input({ required: true }) userId!: string;
-  // @Output() complete = new EventEmitter<string>();
-  
-  // onCompleteForm() {
-  //   this.complete.emit()
-  // }
+  @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+  enteredTitle='';
+  enteredSummary='';
+  enteredDate='';
+
+  onFormSubmit(){
+    this.add.emit({
+      title: this.enteredTitle,
+      summary:this.enteredSummary,
+      date:this.enteredDate
+    })
+  }
+
+  onCancel() {
+    this.cancel.emit();
+  }
 }
