92a13e93c6c54f48b4f34508b6cb0c84857cc0af
Crystal Parker <vertfromage@gmail.com>
2025-01-23 22:23:15 -0400

    practiced angular syntax

---
+++ 06-components-deep-dive/src/app/shared/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[appButton], a[appButton]', //for extending an existing
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
