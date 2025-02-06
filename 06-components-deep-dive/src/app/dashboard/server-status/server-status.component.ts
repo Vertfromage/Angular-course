7db65caf934fd419d0760d884233215b75a06ee1
Crystal Parker <vertfromage@gmail.com>
2025-02-05 20:05:52 -0400

    did a little bit of tutorials

--- 06-components-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-components-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'online'|'offline'| 'unknown'= 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
-  ngOnInit(){
-    setInterval(()=>{
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    const interval = setInterval(() => {
       const rnd = Math.random();
 
-      if(rnd <0.5){
-        this.currentStatus = 'online'
-      } else if(rnd <0.0){
-        this.currentStatus = 'offline'
-      }else{
-        this.currentStatus = 'unknown'
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.0) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
       }
-    },5000)
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
 }
